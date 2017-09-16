37548afd536e4836e269d37a48e01802ee5d1f9c
Wilson Camilo Restrepo <wcrestrepo@gmail.com>
2017-09-16 13:43:59 -0400

    added return types to get_990_urls

--- get_990_urls.py
+++ get_990_urls.py
@@ -7,6 +7,7 @@ einsList = pandas.read_csv('data/incomplete_ein_list.csv');
 
 einsList = einsList[pandas.notnull(einsList['ein'])]
 einsList['url'] = ""
+einsList['return_type'] = ""
 
 for index, row in einsList.iterrows():
     ein =  int(row[1])
@@ -15,9 +16,12 @@ for index, row in einsList.iterrows():
         r = requests.get('http://irs-xml-search.herokuapp.com/search?ein=' + str(ein))
         rjson = json.loads(r.text)
         einsList.loc[index, 'url']  = rjson[0]['url']
+        einsList.loc[index, 'return_type']  = rjson[0]['RETURN_TYPE']
     except:
         print "Error grabbing " + str(ein)
 
 
 einsList = einsList[pandas.notnull(einsList['url'])]
+einsList = einsList[pandas.notnull(einsList['return_type'])]
+
 einsList.to_csv('data/eins_url.csv')
